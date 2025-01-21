01fc13b9900773e930252fbb53e4c9e85f67fcc1
Chauhan Vivek <125378382+vivek7669@users.noreply.github.com>
2025-01-21 14:32:44 +0530

    commit two

--- Exam_Management_System(server)/config/db.js
+++ Exam_Management_System(server)/config/db.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const dbConnector = async() => {
-    const db = await mongoose.connect(process.env.DB_URL);
-    if(!db) return console.log("Database is Not Connected.");
-    return console.log("Database Connected.");
-}
+const dbConnector = async () => {
+  try {
+    const db = await mongoose.connect(process.env.DB_URL, {});
+    
+    // If connection is successful, log this message
+    console.log('Database Connected Successfully.');
+  } catch (error) {
+    // If there's an error connecting to the database, log it
+    console.error('Database Connection Failed:', error.message);
+  }
+};
+
+module.exports = dbConnector;
+
 
 module.exports = dbConnector;
